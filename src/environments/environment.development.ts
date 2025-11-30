13ab6e80c79a729834c14f9b29aa721f1b9fe355
JohnnyGZ41 <33947644+JohnnyGZ41@users.noreply.github.com>
2025-11-30 02:51:38 -0500

    Add files via upload

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: "http://localhost:8080/api/v1",
+
+  usersEndpointPath: "/users",
+  accountsEndpointPath: "/accounts",
+
+  reviewsEndpointPath: "/reviews",
+
+  artsEndpointPath: "/arts",
+  gamesEndpointPath: "/games",
+  audiosEndpointPath: "/audios",
+
+  profilesEndpointPath: "/profiles",
+  portfoliosEndpointPath: "/portfolios",
+  groupProjectsEndpointPath: "/group-projects",
+
+  publicationsEndpointPath: "/publications",
+  answersEndpointPath: "/answers",
+
+  shoppingCartsEndpointPath: "/shopping-carts",
+
+  signInEndpointPath: '/authentication/sign-in',
+  signUpEndpointPath: '/authentication/sign-up',
+};
